40aebac4491e196ab27c8b83faf783c15c1e2c7e
Mukul Rathi <mukul.rathi@gmail.com>
2018-10-13 11:48:08 +0100

    Add lab4 files

---
+++ Intro to C-C++/lab4/list.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "tree.h"
+
+Tree *empty = NULL;
+
+/* BASE EXERCISE */
+
+int tree_member(int x, Tree *tree) { 
+  /* TODO */
+  return 0;
+}
+
+Tree *tree_insert(int x, Tree *tree) { 
+  /* TODO */
+  return empty;
+}
+
+void tree_free(Tree *tree) { 
+  /* TODO */
+}
+
+/* CHALLENGE EXERCISE */ 
+
+void pop_minimum(Tree *tree, int *min, Tree **new_tree) { 
+  /* TODO */
+}
+
+Tree *tree_remove(int x, Tree *tree) { 
+  /* TODO */
+  return empty;
+}

38ad5ad6304ad7406469c26c40034ef37c03eab4
Mukul Rathi <mukul.rathi@gmail.com>
2018-10-13 22:41:35 +0100

    Fix seg fault in Polish calc

--- Intro to C-C++/supervision1/calc.c
+++ Intro to C-C++/supervision1/calc.c
@@ -8,57 +8,61 @@ struct stackNode{
 
 typedef struct stackNode Stack; 
 
-Stack *push(Stack *oldStack, int newVal){
+void   push(Stack **oldStack, int newVal){
 	Stack *newNode = (Stack *) malloc(sizeof(Stack));
 	newNode->value = newVal;
-	newNode->parent = oldStack;
-	return newNode;
+	if(oldStack==NULL){
+		oldStack = &newNode; //initialise it
+		newNode->parent = NULL;
+	}
+	else{
+		newNode->parent = *oldStack;
+	}
+	*oldStack = newNode;
 }
 
-int pop(Stack *oldStack){
-	int val = oldStack->value;
-	Stack *newStack = oldStack->parent;
-	free(oldStack);
-	*oldStack = *newStack;
+int pop(Stack **oldStack){
+	int val = (*oldStack)->value;
+	Stack *newStack = (*oldStack)->parent;
+	free(*oldStack);
+	*oldStack = newStack;
 	return val;
 }
 
 
 int main(int argc, char *argv[]){
-	Stack *st = NULL;
+	Stack **st = (Stack **)malloc(sizeof(Stack *));
  	for(int i=1; i<argc; i++){
 		char curr = *(argv[i]); 
 		if(curr>='0' && curr<='9'){
-			st = push(st, (int) (curr-'0'));		
+			push(st, (int) (curr-'0'));		
 		}
 		else if(curr=='+'){
 			int b = pop(st);
 			int a = pop(st);
-			st = push(st, a+b);
+			push(st, a+b);
 		}
 		
 		else if(curr=='*'){
 			int b = pop(st);
 			int a = pop(st);
-			st = push(st, a*b);
+			push(st, a*b);
 		}
 		
 		else if(curr=='-'){
 			int b = pop(st);
 			int a = pop(st);
-			st = push(st, a-b);
+			push(st, a-b);
 		}
 		
 		else if(curr=='/'){
 			int b = pop(st);
 			int a = pop(st);
-			st = push(st, a/b);
+			push(st, a/b);
 		}
 		
 	}
-	if(st!=NULL){
-		printf("The answer is : %d\n", st->value);
-	}
+	printf("The answer is : %d\n", pop(st));
 	return 0;
 
 }

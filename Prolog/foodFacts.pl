73269a19c39bfbeb240e723ca278cc4060d7818f
Mukul Rathi <mukul.rathi@gmail.com>
2019-03-06 13:34:50 +0000

    Fix everything() pl rule

--- Prolog/foodFacts.pl
+++ Prolog/foodFacts.pl
@@ -1,5 +1,6 @@
 % given list of facts, everything(A) = true if A is a list containing all X such that a(X) is true.
 
+a(potatoes).
 a(ham).
 a(cheese).
 a(eggs).
@@ -7,13 +8,13 @@ a(eggs).
 not(X) :- X, !, fail.
 not(_).
 
-notMember(_, []).
-notMember(X, [X| T]) :- !, fail.
-notMember(X, [A, T]) :- notMember(X, T).
+member([H|_], H).
+member([_|T], H) :- member(T, H).
 
-everything([X]) :- a(X).
-everything([A1, A2]]) :- a(A1), a(A2), \+A1 = A2.
-everything([H|B], B) :- a(H), notMember(H, B).
+%everything([X]) :- a(X).
+%everything([A1, A2]) :- a(A1), a(A2), \+A1 = A2.
 
-everything(A, Found) :- a(X), notMember(X,A),!, everything(A, [X|Found]).
+%everything([H|B], B) :- a(H), notMember(H, B).
+
+everything(A, Found) :- a(X), not(member(Found,X)),!, everything(A, [X|Found]).
 everything(A, A).
